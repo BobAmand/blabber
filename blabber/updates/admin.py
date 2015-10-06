f4faff4008c80bac83c4e5897c8920c28a55dc1b
Bob Amand <biotmg@nc.rr.com>
2015-10-06 12:30:36 -0400

    Status updates

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Status, Favorite
+
 
 # Register your models here.
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', 'test', 'posted_at']
+
+
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Favorite)
